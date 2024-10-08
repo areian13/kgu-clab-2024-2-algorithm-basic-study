0f9243136d399a4f902311096d46fc4415a4eae6
wodls <wodls1593@gmail.com>
2024-10-08 20:15:32 +0900

    1주차과제제출_3

--- 1주차/과제/wodls1593/6.소수_1.cpp
+++ 1주차/과제/wodls1593/6.소수_1.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define MAX 10000
 bool Squrt(int n){
     if(n == 1) return false;
+    if(n == 2) return true;
     for (int i = 2; i * i <= n ; i++)
     {
         if(n % i==0)
@@ -20,13 +21,18 @@ int main() {
     int sum =0;
     int min =0;
     cin >> M >> N;
-
-    for(int i = M; i * i<=N; i++){
+    
+    for(int i = M; i <=N; i++){
         if(Squrt(i)){
             sum += i;
-            (min == 0) ? min = i : i=min;
+            (min == 0) ? min = i : min=min;
         }
 
+    }
+    if(sum == 0)
+    {
+        cout << "-1";
+        return 0;
     }
     cout << sum <<"\n"<<min;
 }
